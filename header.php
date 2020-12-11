41df745906911c5302ca075c3de7eed95b1cd7e6
nmhieu0303 <nmhieu03032000@gmail.com>
2020-12-11 09:13:21 +0700

    Fix header for PC screen

--- header.php
+++ header.php
@@ -33,9 +33,8 @@
 
    <body>
 
-     <!-- PC -->
+     <!-- HEADER-PC -->
      <div class="header container-fluid hidden-xs hidden-sm">
-       <!-- HEADER -->
        <div class="row">
          <ul class="header__menu list-unstyled d-flex mb-0 w-100 justify-content-end">
            <li class="header__item"><a class="header__item--link" href="https://ananas.vn/search-order"><i class="fas fa-clipboard-list header__item-icon"></i>Tra cứu đơn hàng</a></li>
@@ -52,177 +51,84 @@
        </div>
 
        <!-- NAVBAR -->
-
        <div class="row">
          <nav class=" navbar navbar-expand-lg navbar-light container">
-           <a class="navbar-brand" href="#"><img src="./assets/img/Logo_Ananas_Header.svg"></a>
-           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon"></span>
-           </button>
+           <!-- LOGO -->
+           <a class="navbar-brand" href="index.php"><img src="./assets/img/Logo_Ananas_Header.svg"></a>
 
+           <!-- MENU -->
            <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="nav navbar-nav m-auto">
                <li class=" nav-item dropdown">
-                 <a class="nav-link dropdown-toggle" href="https://ananas.vn/product-list/?gender=men" id="navbarDropdown">SẢN PHẨM <span class="caret"></span></a>
+                 <a class="nav-link dropdown-toggle" href="login.php" id="navbarDropdown">SẢN PHẨM <span class="caret"></span></a>
                  <!-- Sub menu 1 -->
                  <ul class="dropdown-menu">
                    <li class="dropdown-item dropdown">
-                     <a class="style1-title" href="/product-list/?gender=men">CHO NAM</a>
+                     <a href="#">CHO NAM</a>
                      <i class="fas fa-chevron-right dropdown-icon"></i>
-                     <!-- Sub menu 2 -->
+                     <!-- Sub menu level 2 -->
                      <ul class="dropdown-menu">
-                       <li class="dropdown-item "><a class="style1-title" href="/product-list/?gender=men">Giày</a></li>
-                       <li class="dropdown-item"><a class="style1-title" href="/product-list/?gender=women">Áo</a></li>
-                       <li class="dropdown-item"><a class="style1-title" href="/promotion/clearance-sale/">Phụ Kiện</a></li>
-                       <li class="dropdown-item "><a class="style1-title" href="/product-list?gender=men,women&category=top,bottom,accessories&attribute=">THỜI TRANG & PHỤ KIỆN</a></li>
+                       <li class="dropdown-item "><a href="#">Giày</a></li>
+                       <li class="dropdown-item"><a href="#">Áo</a></li>
+                       <li class="dropdown-item"><a href="#">Phụ Kiện</a></li>
+                       <li class="dropdown-item "><a href="#">THỜI TRANG & PHỤ KIỆN</a></li>
                      </ul>
-                  </li>
-                   <li class="dropdown-item"><a class="style1-title" href="/product-list/?gender=women">CHO NỮ</a></li>
-                   <li class="dropdown-item"><a class="style1-title" href="/promotion/clearance-sale/">OUTLET SALE</a></li>
+                   </li>
+                   <li class="dropdown-item dropdown">
+                     <a href="#">CHO Nữ</a>
+                     <i class="fas fa-chevron-right dropdown-icon"></i>
+                     <!-- Sub menu level 2 -->
+                     <ul class="dropdown-menu">
+                       <li class="dropdown-item "><a href="#">Giày</a></li>
+                       <li class="dropdown-item"><a href="#">Áo</a></li>
+                       <li class="dropdown-item"><a href="#">Phụ Kiện</a></li>
+                       <li class="dropdown-item "><a href="#">THỜI TRANG & PHỤ KIỆN</a></li>
+                     </ul>
+                   </li>
+                   <li class="dropdown-item"><a href="#">OUTLET SALE</a></li>
                    <li class="dropdown-item ">
-                     <a class="style1-title" href="/product-list?gender=men,women&category=top,bottom,accessories&attribute=">THỜI TRANG & PHỤ KIỆN</a>
-                     
+                     <a href="#">THỜI TRANG & PHỤ KIỆN</a>
                    </li>
                  </ul>
                </li>
 
                <li class="line"></li>
                <li class=" nav-item dropdown">
-                 <a class="nav-link dropdown-toggle" href="https://ananas.vn/product-list/?gender=men" id="navbarDropdown">NAM <span class="caret"></span></a>
-                 <ul class="dropdown-menu style2">
-                   <li>
-                     <a href="#" class="title">NỔI BẬT</a>
-
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href=""></a>
-                     <a href="/product-list?gender=men&category=&attribute=best-seller">Best Seller</a>
-                     <a href="/product-list?gender=men&category=&attribute=new-arrival">New Arrival</a>
-                     <a href="/product-list?gender=men&category=&attribute=sale-off">Sale off</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Bộ sưu tập</a>
-                     <a href="/product-list?gender=&category=&attribute=pineapple-or-ananas">Pineapple Or Ananas</a>
-                     <a href="/product-list?gender=&category=&attribute=corluray">Corluray</a>
-                     <a href="/product-list?gender=men,women&category=&attribute=bst-unsettling">Unsettling</a>
-                     <a href="/product-list?gender=&category=&attribute=irrelevent">Irrelevent</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Collaboration</a>
-                     <a href="/product-list/?gender=&category=&attribute=ananas-lucky-luke">Ananas x Lucky Luke</a>
-                     <a class="blank">&nbsp;</a>
-                   </li>
-                   <li class="style2-divider"></li>
-                   <li>
-                     <a href="#" class="title">GIÀY</a>
-
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Dòng sản phẩm</a>
-                     <a href="/product-list?gender=men&category=&attribute=basas">Basas</a>
-                     <a href="/product-list?gender=men&category=&attribute=vintas">Vintas</a>
-                     <a href="/product-list?gender=men&category=&attribute=urbas">Urbas</a>
-                     <a href="/product-list?gender=men&category=&attribute=pattas">Pattas</a>
-                     <a href="/product-list?gender=men&category=&attribute=creas">Creas</a>
-                     <a href="/product-list?gender=men&category=&attribute=track-6">Track 6</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Style</a>
-                     <a href="/product-list?gender=men&category=&attribute=high-top?gender=men&category=&attribute=high-top">High Top</a>
-                     <a href="/product-list">Low Top</a>
-                     <a href="/product-list?gender=men&category=&attribute=slip-on">Slip-on</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="/product-list?gender=men&category=shoes&attribute=">Tất cả giày</a>
-                     <a class="blank">&nbsp;</a>
-                   </li>
-                   <li>
-                     <a href="/product-list?gender=men&category=top,bottom,accessories&attribute=" class="title">THỜI TRANG & PHỤ KIỆN</a>
-
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Nửa trên</a>
-                     <a href="/product-list?gender=men&category=&attribute=basic-tee-tron">Basic Tee</a>
-                     <a href="/product-list/?gender=&category=&attribute=graphic-tee">Graphic tee</a>
-                     <a href="/product-list?gender=&category=&attribute=sweatshirt">Sweatshirt</a>
-                     <a href="/product-list?gender=&category=&attribute=hoodie">Hoodie</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Phụ kiện</a>
-                     <a href="/product-list?gender=men&category=&attribute=hat">Nón</a>
-                     <a href="/product-list?gender=men&category=&attribute=shoelaces">Dây giày</a>
-                     <a href="/product-list?gender=men&category=&attribute=socks">Vớ</a>
-                     <a href="/product-list?gender=men&category=&attribute=backpack">Ba lô & Túi</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="/product-list?gender=men&category=top,bottom,accessories&attribute=">Xem tất cả</a>
-                     <a class="blank">&nbsp;</a>
-                   </li>
-                   <div class="style2-des"><a href="/coming-soon/">MỌI NGƯỜI THƯỜNG GỌI CHÚNG TÔI LÀ <span class="highlight">DỨA</span> !</a></div>
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown">NAM <span class="caret"></span></a>
+                 <ul class="dropdown-menu">
+                   <li class="dropdown-item "><a href="#">Giày</a></li>
+                   <li class="dropdown-item"><a href="#">Áo</a></li>
+                   <li class="dropdown-item"><a href="#">Phụ Kiện</a></li>
+                   <li class="dropdown-item "><a href="#">THỜI TRANG & PHỤ KIỆN</a></li>
                  </ul>
                </li>
 
                <li class="line"></li>
                <li class=" nav-item dropdown">
-                 <a class="nav-link dropdown-toggle" href="https://ananas.vn/product-list/?gender=men" id="navbarDropdown">NỮ<span class="caret"></span></a>
-                 <ul class="dropdown-menu style2">
-                   <li>
-                     <a href="" class="title">NỔI BẬT</a>
-
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href=""></a>
-                     <a href="/product-list?gender=women&category=&attribute=best-seller">Best Seller</a>
-                     <a href="/product-list?gender=women&category=&attribute=new-arrival">New Arrival</a>
-                     <a href="/product-list?gender=women&category=&attribute=sale-off">Sale-off</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Bộ sưu tập</a>
-                     <a href="/product-list?gender=&category=&attribute=pineapple-or-ananas">Pineapple Or Ananas</a>
-                     <a href="/product-list?gender=&category=&attribute=corluray">Corluray</a>
-                     <a href="/product-list?gender=men,women&category=&attribute=bst-unsettling">Unsettling</a>
-                     <a href="/product-list?gender=&category=&attribute=irrelevent">Irrelevent</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Collaboration</a>
-                     <a href="/product-list/?gender=&category=&attribute=ananas-lucky-luke">Ananas x Lucky Luke</a>
-                     <a class="blank">&nbsp;</a>
-                   </li>
-                   <li class="style2-divider"></li>
-                   <li>
-                     <a href="" class="title">GIÀY</a>
-
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Dòng sản phẩm</a>
-                     <a href="/product-list?gender=women&category=&attribute=basas">Basas</a>
-                     <a href="/product-list?gender=women&category=&attribute=vintas">Vintas</a>
-                     <a href="/product-list?gender=women&category=&attribute=urbas">Urbas</a>
-                     <a href="/product-list?gender=women&category=&attribute=pattas">Pattas</a>
-                     <a href="/product-list?gender=women&category=&attribute=creas">Creas</a>
-                     <a href="/product-list?gender=women&category=&attribute=track-6">Track 6</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Style</a>
-                     <a href="/product-list?gender=men&category=&attribute=high-top?gender=women&category=&attribute=high-top">High Top</a>
-                     <a href="/product-list?gender=men&category=&attribute=high-top?gender=women&category=&attribute=low-top">Low Top</a>
-                     <a href="/product-list?gender=men&category=&attribute=high-top?gender=women&category=&attribute=slip-on">Slip-on</a>
-                     <a class="blank">&nbsp;</a>
-                   </li>
-                   <li>
-                     <a href="/product-list?gender=women&category=top,bottom,accessories&attribute=" class="title">THỜI TRANG & PHỤ KIỆN</a>
-
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Nửa trên</a>
-                     <a href="/product-list?gender=women&category=&attribute=basic-tee-tron">Basic Tee</a>
-                     <a href="/product-list/?gender=&category=&attribute=graphic-tee">Graphic Tee</a>
-                     <a href="/product-list?gender=&category=&attribute=sweatshirt">Sweatshirt</a>
-                     <a href="/product-list?gender=&category=&attribute=hoodie">Hoodie</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="#">Phụ kiện</a>
-                     <a href="/product-list?gender=women&category=&attribute=hat">Nón</a>
-                     <a href="/product-list?gender=women&category=&attribute=shoelaces">Dây giày</a>
-                     <a href="/product-list?gender=men&category=&attribute=socks">Vớ</a>
-                     <a href="/product-list?gender=women&category=&attribute=backpack">Ba lô & Túi</a>
-                     <a class="blank">&nbsp;</a>
-                     <a class="title1" href="/product-list?gender=women&category=top,bottom,accessories&attribute=">Xem tất cả</a>
-                     <a class="blank">&nbsp;</a>
-                   </li>
-                   <div class="style2-des"><a href="/coming-soon/">MỌI NGƯỜI THƯỜNG GỌI CHÚNG TÔI LÀ <span class="highlight">DỨA</span> !</a></div>
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown">NỮ<span class="caret"></span></a>
+                 <ul class="dropdown-menu">
+                   <li class="dropdown-item "><a href="#">Giày</a></li>
+                   <li class="dropdown-item"><a href="#">Áo</a></li>
+                   <li class="dropdown-item"><a href="#">Phụ Kiện</a></li>
+                   <li class="dropdown-item "><a href="#">THỜI TRANG & PHỤ KIỆN</a></li>
                  </ul>
                </li>
 
                <li class="line"></li>
-               <li><a class="nav-link" href="https://ananas.vn/promotion/clearance-sale/">SALE OFF</a></li>
+               <li class=" nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown">SALE OFF<span class="caret"></span></a>
+                 <ul class="dropdown-menu">
+                   <li class="dropdown-item "><a href="#">10%</a></li>
+                   <li class="dropdown-item"><a href="#">25%</a></li>
+                   <li class="dropdown-item"><a href="#">40%</a></li>
+                   <li class="dropdown-item "><a href="#">50%</a></li>
+                 </ul>
+               </li>
+
                <li class="line"></li>
-               <li><a class="nav-link" href="https://ananas.vn/discoveryou"><img src="./assets/img/DiscoverYOU.svg"></a></li>
+               <li><a class="nav-link" href="#"><img src="./assets/img/DiscoverYOU.svg"></a></li>
              </ul>
+             <!-- END MENU -->
 
              <!-- SEARCH BOX -->
              <form class="col-lg-3 col-12 search-box form-inline my-2 my-lg-0 ">
@@ -230,8 +136,9 @@
                <button class="search-box__btn" type="submit"><i class="fas fa-search"></i></button>
              </form>
            </div>
-           <!-- User -->
+           <!-- END MENU -->
 
+           <!-- BUTTON  AVATAR USER -->
            <?php if ($currentUser) : ?>
              <a href="profile.php" class="btn-profile rounded-circle ml-1">
                <img class="btn-profile--img " src="avatar.php?id=<?php echo $currentUser['id']; ?>" alt="">
@@ -241,15 +148,14 @@
                <img class="btn-profile--img " src="avatar.php?id=<?php echo $currentUser['id']; ?>" alt="">
              </a>
            <?php endif; ?>
-
+           <!-- END BUTTON AVATAR USER -->
 
          </nav>
-
        </div>
+       <!-- END NAVBAR -->
 
-
+       <!-- HOT NEWS SLIDE -->
        <div class="row">
-
          <div class="hot-news-cont">
            <div class="hot-news-slide">
              <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
@@ -275,10 +181,17 @@
              </div>
            </div>
          </div>
-
        </div>
-       <div id="content" style="min-height:100vh;">
-         <div class="container pt-3">
-           <?php if ($title != "Home") : ?>
-             <h1 class="display-4 text-center font-weight-normal"><?php echo $title ?></h1>
-           <?php endif; ?>
+       <!-- END HOT NEWS SLIDE -->
+
+     </div>
+     <!-- END HEADER-PC -->
+
+     
+
+     
+     <div id="content" style="min-height:100vh;">
+       <div class="container pt-3">
+         <?php if ($title != "Home") : ?>
+           <h1 class="display-4 text-center font-weight-normal"><?php echo $title ?></h1>
+         <?php endif; ?>
