1357eb8b93aad6a91a67ee84418fe38b0e47d41f
mongchuyen <mongchuyen0102@gmail.com>
2020-12-13 16:27:09 +0700

    chuyen fixed footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,24 @@
 <!-- Start footer -->
-
+<div class="footer container ">
+ <div class="row d-flex text-center mt-5">
+  <div class="col-md-3  ">
+   <h3>SẢN PHẨM</h3>
+   <p>Giày nữ</p>
+   <p>Giày nam</p>
+   <p>Thời trang</p>
+   <p>Sale-off</p>
+  </div>
+  <div class="col-md-3 ">
+   <h3>CÔNG TY</h3>
+  </div>
+  <div class="col-md-3 ">
+   <h3>HỖ TRỢ</h3>
+  </div>
+  <div class="col-md-3 ">
+   <h3>LIÊN HỆ</h3>
+  </div>
+ </div>
+</div>
 
 <!-- end footer -->
 
