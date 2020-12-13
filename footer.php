c964fa7ff2a96abbb4d901027d7fd731d6ca893d
mongchuyen <mongchuyen0102@gmail.com>
2020-12-13 15:38:45 +0700

    chuyen fixed 13/12/20

--- footer.php
+++ footer.php
@@ -1,17 +1,19 @@
-          
-        </div>
-    </div>
-    <script>
-        function loadFile(event){
-            var imgInput = document.getElementById("img-preview");
-            imgInput.src =URL.createObjectURL(event.target.files[0]);
-        }
-        // In your Javascript (external .js resource or <script> tag)
-        $(document).ready(function() {
-                 $('.js-example-basic-single').select2();
-        }); 
+<!-- Start footer -->
+
+
+<!-- end footer -->
+
+<script>
+function loadFile(event) {
+ var imgInput = document.getElementById("img-preview");
+ imgInput.src = URL.createObjectURL(event.target.files[0]);
+}
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+ $('.js-example-basic-single').select2();
+});
 </script>
-        <script src="/assets/js/main.js"></script>
+<script src="/assets/js/main.js"></script>
 </body>
 
 </htm>
--- formLogin.php
+++ formLogin.php
@@ -1,16 +1,20 @@
 <form action="login.php" method="POST">
-    <div class="form-group">
-        <label>Username</label>
-        <input type="text" class="form-control" name="username" required>
-    </div>
-    <div class="form-group">
-        <label>Password</label> </label>
-        <input type="password" class="form-control" name="password" required>
-    </div>
-    <div class="d-flex justify-content-between m-4">
-        <a href="forgetPass.php">Forget password ??</a>
-        <a href="register.php">Register</a>
-    </div>
-    <button type="submit" class="btn btn-primary">Login</button>
-    <div class = "mt-4"><small>&copy; 2020 Nguyễn Minh Hiếu</small></div>
+ <div class="login" style="width:500px; margin:0 auto;">
+  <div class="form-group" style="width: 100%;">
+   <label>Username</label>
+   <input type="text" class="form-control" name="username" required>
+  </div>
+  <div class="form-group" style="width: 100%;">
+   <label>Password</label> </label>
+   <input type="password" class="form-control" name="password" required>
+  </div>
+  <div class="d-flex justify-content-between mb-4">
+   <a href="forgetPass.php" style="color: #F26D3F">Forget password ??</a>
+   <a href="register.php " style="color: #F26D3F">Register</a>
+  </div>
+
+  <button type="submit" class="btn" style="background: #F26D3F;color:#fff">Login</button>
+  <div class="mt-4"><small>&copy; 2020 Nguyễn Minh Hiếu</small></div>
+ </div>
+
 </form>
